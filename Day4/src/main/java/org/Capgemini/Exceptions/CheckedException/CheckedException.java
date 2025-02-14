d54851a66eb9e6d0e526febc7d33960a2982eaa2
Ayush2020 <ayushgiri2021itc@gmail.com>
2025-02-14 14:51:34 +0530

    [Ayush] Checked Exception

--- Day4/src/main/java/org/Capgemini/Exceptions/CheckedException/CheckedException.java
+++
@@ -1,3 +0,0 @@
-package org.Capgemini.Exceptions.CheckedException;
-
-
---
+++ Day4/src/main/java/org/Capgemini/Exceptions/CheckedException/ReadFile.java
@@ -0,0 +1,24 @@
+package org.Capgemini.Exceptions.CheckedException;
+
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+
+public class ReadFile {
+
+    public static void main(String[] args) {
+        String filePath = "data.txt"; // Specify the file name
+
+        try (BufferedReader reader = new BufferedReader(new FileReader(filePath))) {
+            String line;
+            // Reading the file line by line
+            while ((line = reader.readLine()) != null) {
+                System.out.println(line);
+            }
+        } catch (IOException e) {
+            // Handle the exception if the file doesn't exist or another I/O issue occurs
+            System.out.println("File Not found");
+        }
+    }
+}
+
