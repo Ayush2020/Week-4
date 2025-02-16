cd74b77e2b78644ea021b46e07fd2640eba5aa89
Ayush2020 <ayushgiri2021itc@gmail.com>
2025-02-16 14:21:59 +0530

    [Ayush] add . Testing a Calculator Class

--- Day5/src/main/java/org/Capgemini/JUnit/Calculator/Calculator.java
+++ Day5/src/main/java/org/Capgemini/JUnit/Calculator/Calculator.java
@@ -24,33 +24,5 @@ public class Calculator {
         }
         return (double) a / b;
     }
-
-    // Main method for manual testing
-    public static void main(String[] args) {
-        Calculator calc = new Calculator();
-
-        // Test Addition
-        System.out.println("Addition (5 + 5): " + calc.add(5, 5));
-
-        // Test Subtraction
-        System.out.println("Subtraction (10 - 3): " + calc.subtract(10, 3));
-
-        // Test Multiplication
-        System.out.println("Multiplication (4 * 5): " + calc.multiply(4, 5));
-
-        // Test Division
-        try {
-            System.out.println("Division (10 / 2): " + calc.divide(10, 2));
-        } catch (ArithmeticException e) {
-            System.out.println(e.getMessage());
-        }
-
-        // Test Division by Zero
-        try {
-            System.out.println("Division (10 / 0): " + calc.divide(10, 0));
-        } catch (ArithmeticException e) {
-            System.out.println(e.getMessage());
-        }
-    }
 }
 
