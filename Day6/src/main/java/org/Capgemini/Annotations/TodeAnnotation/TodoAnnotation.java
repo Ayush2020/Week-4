59ffb1378cac7c5bca865ec3d914bbeb14f4219b
Ayush2020 <ayushgiri2021itc@gmail.com>
2025-02-18 12:08:37 +0530

    [Ayush] add . Todo Annotaion

--- Day6/src/main/java/org/Capgemini/Annotations/TodeAnnotation/TodoAnnotation.java
+++ Day6/src/main/java/org/Capgemini/Annotations/TodeAnnotation/TodoAnnotation.java
@@ -8,6 +8,7 @@ import java.lang.reflect.Method;
 
 public class TodoAnnotation {
 
+
     // Define the @Todo annotation
     @Target(ElementType.METHOD) // Can be applied to methods
     @Retention(RetentionPolicy.RUNTIME) // Retained at runtime for reflection
